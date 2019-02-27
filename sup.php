98dde8a7ccc54066c6c671764b0b6fbdcab87759
Christopher M. Amador <45224041+taktiks@users.noreply.github.com>
2019-02-27 13:11:08 +0800

    Add files via upload

---
+++ sup.php
@@ -0,0 +1,22 @@
+<?php
+
+	$fname = $_POST['first'];
+	$lname = $_POST['last'];
+	$cname = $_POST['comp'];
+	$email = $_POST['email'];
+	$num = $_POST['number'];
+	$sub = $_POST['ad'];
+	$ch = $_POST['dot'];
+
+
+	echo "<h3>".$fname. " " .$lname."</h3>";
+	echo "<h3>".$cname."</h3>";
+	echo "<h3>".$email."</h3>";
+	echo "<h3>".$num."</h3>";
+	echo "<h3>".$sub."</h3>";
+	echo "<h3>".$ch."</h3>";
+	
+
+
+
+?>
